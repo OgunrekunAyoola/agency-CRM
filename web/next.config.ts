f65c73d6cb8f451c9c3567c965b171020ebd79dd
OgunrekunAyoola <oreofeogunrekun@gmail.com>
2026-04-20 14:30:30 +0100

    vps setup in config

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const apiBase = process.env.NEXT_PUBLIC_API_BASE_URL || '';
 const normalizedApiBase = apiBase
@@ -8,15 +9,17 @@ const normalizedApiBase = apiBase
   : '';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  poweredByHeader: false,
+  compress: false,
   output: 'standalone',
+  outputFileTracingRoot: path.join(__dirname, "../"),
   async rewrites() {
     // Prevent circular rewrites if normalizedApiBase is empty or the same as the current host
     if (!normalizedApiBase) {
       return [];
     }
-    
+
     return [
       {
         source: '/api/:path*',
@@ -45,6 +48,10 @@ const nextConfig: NextConfig = {
           {
             key: 'Permissions-Policy',
             value: 'camera=(), microphone=(), geolocation=()',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains',
           },
           {
             key: 'Content-Security-Policy',
